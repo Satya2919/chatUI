a7b593b808edfb4cb6cfd5906975e4dcc96b1685
satyam <sat.tiwari21@gmail.com>
2020-06-16 16:52:07 +0530

    Initial commit

---
+++ lib/data/data.dart
@@ -0,0 +1,9 @@
+final prevention = [
+  {'assets/images/wash_hands.png': 'Clean your\nhands before\nand after\n dialysis '},
+  {'assets/images/mask.png': 'Wear a\nfacemask\n when config.\n device'},
+  {'assets/images/chatbot.png': 'Talk to\n chatbot\n whenever\n necessary'},
+  {'assets/images/schedule.png': 'Follow the\n schedule\n strictly'}
+
+];
+
+final covidUSADailyNewCases = [12.17, 11.15, 10.02, 11.21, 13.83, 14.16, 14.30];
