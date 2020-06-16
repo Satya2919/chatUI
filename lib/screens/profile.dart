474795aab5231d7573a90a53091d82765001c32f
satyam <sat.tiwari21@gmail.com>
2020-06-16 23:52:12 +0530

    Updated Profile

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,13 +1,181 @@
+import 'dart:ui';
+
+import 'package:covid/config/palette.dart';
 import 'package:covid/widgets/custom_app_bar.dart';
 import 'package:covid/widgets/navbar.dart';
 import 'package:flutter/material.dart';
 
-class profile extends StatelessWidget {
+
+class profile extends StatefulWidget {
+  @override
+  _profileState createState() => _profileState();
+}
+
+class _profileState extends State<profile> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: CustomDrawer(),
-      appBar: CustomAppBar(),
-    );
+    return new Scaffold(
+        drawer: CustomDrawer(),
+        appBar: AppBar(
+          title: Text(
+            'Profile',
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Palette.primaryColor.withOpacity(0.7),
+        ),
+        body: new Stack(
+          children: <Widget>[
+
+            ClipPath(
+              child: Container(color: Palette.primaryColor.withOpacity(0.7)),
+              clipper: getClipper(),
+            ),
+            Positioned(
+                width: 350.0,
+                top: MediaQuery.of(context).size.height / 5,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                        width: 150.0,
+                        height: 150.0,
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    'https://banner2.cleanpng.com/20181117/rr/kisspng-logo-user-viva-life-style-information-design-5bf0a6a16b8413.8854598315424979534404.jpg'),
+                                fit: BoxFit.cover),
+                            borderRadius: BorderRadius.all(Radius.circular(75.0)),
+                            boxShadow: [
+                              BoxShadow(blurRadius: 7.0, color: Colors.black)
+                            ])),
+                    SizedBox(height: 30.0),
+                    Text(
+                      'Satyam Tiwari',
+                      style: TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Montserrat'),
+                    ),
+                    SizedBox(height: 15.0),
+                    Text(
+                      'Phone Number-\n'
+                          '7362XXXX9',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontStyle: FontStyle.italic,
+                          fontFamily: 'Montserrat'),
+                    ),
+                    SizedBox(height: 15.0),
+                    Text(
+                      'Address',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontStyle: FontStyle.italic,
+                          fontFamily: 'Montserrat'),
+                    ),
+                    SizedBox(height: 15.0),
+                    Text(
+                      'Age-21',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontStyle: FontStyle.italic,
+                          fontFamily: 'Montserrat'),
+                    ),
+                    SizedBox(height: 25.0),
+                    Container(
+                        height: 30.0,
+                        width: 95.0,
+                        child: Material(
+                          borderRadius: BorderRadius.circular(20.0),
+                          shadowColor: Colors.greenAccent,
+                          color: Colors.green,
+                          elevation: 7.0,
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: Center(
+                              child: Text(
+                                'Edit Name',
+                                style: TextStyle(color: Colors.white, fontFamily: 'Montserrat'),
+                              ),
+                            ),
+                          ),
+                        )),
+                    SizedBox(height: 25.0),
+                    Container(
+                        height: 30.0,
+                        width: 95.0,
+                        child: Material(
+                          borderRadius: BorderRadius.circular(20.0),
+                          shadowColor: Colors.redAccent,
+                          color: Colors.red,
+                          elevation: 7.0,
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: Center(
+                              child: Text(
+                                'Log out',
+                                style: TextStyle(color: Colors.white, fontFamily: 'Montserrat'),
+                              ),
+                            ),
+                          ),
+                        ))
+                  ],
+                )),
+
+          ],
+        ));
   }
 }
+
+class getClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = new Path();
+
+    path.lineTo(0.0, size.height / 1.9);
+    path.lineTo(size.width + 125, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    return true;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
